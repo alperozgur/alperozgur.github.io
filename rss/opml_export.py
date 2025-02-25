72987489a52434aa509aa8c2575bdee2937dbd0c
Alper Özgür <6560307+alperozgur@users.noreply.github.com>
2025-02-25 21:52:31 +0100

    opml

--- rss/opml_export.py
+++ rss/opml_export.py
@@ -1,28 +1,46 @@
 import os
 import xml.etree.ElementTree as ET
+import sqlite3
+from feedgen.feed import FeedGenerator
+from datetime import datetime
+import pytz
 
-domain = "https://alperozgur.github.io/"  # Change to your domain
-def create_opml(directory, output_file):
-    # Create the OPML structure
-    opml = ET.Element("opml", version="2.0")
-    head = ET.SubElement(opml, "head")
-    ET.SubElement(head, "title").text = "Köşe Yazarları RSS Listesi"
-    body = ET.SubElement(opml, "body")
-    
-    # Iterate through XML files in the given directory
-    for file in os.listdir(directory):
-        if file.endswith(".xml"):
-            file_path = os.path.join(directory, file)
-            ET.SubElement(body, "outline", text=file, type="link", url=domain+file_path)
-    
-    # Convert tree to a string and write to file
-    tree = ET.ElementTree(opml)
-    with open(output_file, "wb") as f:
-        tree.write(f, encoding="utf-8", xml_declaration=True)
-    
-    print(f"OPML file created: {output_file}")
+DB_PATH = "./rss/articles.db"
+TB_ARTICLES = "articles"
+TB_AUTHORS = "authors"
+rss_path = "rss/xml"
+domain = "https://alperozgur.github.io/rss/xml/"  # Change to your domain
+
+def fetch_authors():
+    try:
+        with sqlite3.connect(DB_PATH) as conn:
+            cur = conn.cursor()
+            cur.execute(f"SELECT short,author FROM {TB_AUTHORS}")
+            authors = cur.fetchall()
+
+        if not authors:
+            print("No authors found in the database.")
+            return
+            # Create the OPML structure
+        opml = ET.Element("opml", version="2.0")
+        head = ET.SubElement(opml, "head")
+        ET.SubElement(head, "title").text = "Köşe Yazarları RSS Listesi"
+        body = ET.SubElement(opml, "body")
+        
+        for short, author in authors:
+            ET.SubElement(body, "outline", text=author, type="link", url=domain+short+".xml")
+        # Convert tree to a string and write to file
+        tree = ET.ElementTree(opml)
+        with open(output_file, "wb") as f:
+            tree.write(f, encoding="utf-8", xml_declaration=True)
+        
+        print(f"OPML file created: {output_file}")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+
 
 # Example usage
 directory = "rss/xml"  # Change to your XML folder
 output_file = "rss/xml/authors.opml"
-create_opml(directory, output_file)
+fetch_authors()
+
